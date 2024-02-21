6f8b10976111b08712636a199c122c0dbdbbad1a
Karl Svartholm <gorbiz@gmail.com>
2024-02-21 20:25:58 +0000

    fix broken /list route

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import bodyParser from 'body-parser'
 import sqlite3 from 'sqlite3'
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const router = express.Router()
 
